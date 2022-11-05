0b64dd388e59a8f7064a2853cffd84f4d0106e17
Danilo Biladóniga <danilobiladoniga@gmail.com>
2022-11-05 20:53:52 -0300

    Fix: tcp helper

--- LkdinSystem/Protocol/tcpHelper.cs
+++ LkdinSystem/Protocol/tcpHelper.cs
@@ -19,41 +19,16 @@ namespace Protocol
 
         public async Task SendAsync(byte[] data)
         {
-            //obtener datos a enviar
-            //obtener el largo de los datos del mensaje 
-            byte[] dataLength = BitConverter.GetBytes(data.Length);
-            //enviar el largo 
-            await _networkStream.WriteAsync(dataLength, 0, Protocol.Constants.WordLength).ConfigureAwait(false);
-            //Enviar el mensaje
             await _networkStream.WriteAsync(data, 0, data.Length).ConfigureAwait(false);
         }
 
         public async Task<byte[]> ReceiveAsync(int length)
         {
-            //recibo el largo del mensaje
-            int offset = 0; //offset
-            byte[] dataLength = new byte[length];
-            while (offset < Protocol.Constants.WordLength)
+            int offset = 0;
+            var data = new byte[length];
+            while (offset < length)
             {
-                //para recibir o leer el buffer si o si tenemos que llevar a mano lo que se recibe
-                // y cotnrolar que recibimos lo que necesitamos
-                var count = Protocol.Constants.WordLength - offset;
-                int recived = await _networkStream.ReadAsync(dataLength, offset, count).ConfigureAwait(false);
-                if (recived == 0)
-                {
-                    _networkStream.Close();
-                    throw new Exception("Connection ends");
-                }
-                offset += recived;
-            }
-            var largo = BitConverter.ToInt32(dataLength);
-
-            //recibo el mensaje
-            var data = new byte[largo];
-            offset = 0;
-            while (offset < largo)
-            {
-                int count = largo - offset;
+                int count = length - offset;
                 int recived = await _networkStream.ReadAsync(data, offset, count).ConfigureAwait(false);
                 if (recived == 0)
                 {
