101f3b3bd9217f6ec3561ad717dbc07df462e4ac
Romina Giaccio <giacciopromina@gmail.com>
2022-11-23 17:17:13 -0300

    feature: adding skills to dtoProfiles

--- LkdinSystem/GrpcServerProgram/Services/GreeterService.cs
+++ LkdinSystem/GrpcServerProgram/Services/GreeterService.cs
@@ -18,56 +18,58 @@ namespace GrpcServerProgram.Services
             });
         }
 
-        public Task<MessageReply> PostUser(UserDTO request, ServerCallContext context)
+        public override Task<MessageReply> PostUser(UserDTO request, ServerCallContext context)
         {
             //BusinessLogic session = BusinessLogic.GetInstance();
             Console.WriteLine("Se intenta crear usuario con nombre {0}", request.Name);
-            //string message = "Se intento crear usuario" + request.Name;//session.SaveNewUserAsync(string message);//(request.Name);;
+            //string message = "Se intento crear usuario" + request.Name;//session.SaveNewUserAsync(string message);//(request.Name);
             var message = serverCommands.SaveNewUserAsync(request.Id +"/#" + request.Name + "/#" + request.Email + "/#" + request.CurrentState);
             return Task.FromResult(new MessageReply { Message = message.Result });
         }
 
-        public Task<MessageReply> PutUser(UserDTO request, ServerCallContext context)
+        public override Task<MessageReply> PutUser(UserDTO request, ServerCallContext context)
         {
             Console.WriteLine("Se intenta actualizar usuario con nombre {0}", request.Name);
             var message = serverCommands.UploadUserAsync(request.Id + "/#" + request.Name + "/#" + request.Email + "/#" + request.CurrentState);
             return Task.FromResult(new MessageReply { Message = message.Result });
         }
 
-        public Task<MessageReply> DeleteUser(UserDTO request, ServerCallContext context)
+        public override Task<MessageReply> DeleteUser(UserDTO request, ServerCallContext context)
         {
             Console.WriteLine("Se intenta eliminar usuario con nombre {0}", request.Name);
             var message = serverCommands.DeleteUserAsync(request.Id + "/#" + request.Name + "/#" + request.Email + "/#" + request.CurrentState);
             return Task.FromResult(new MessageReply { Message = message.Result });
         }
 
-        public Task<MessageReply> PostProfile(ProfileDTO request, ServerCallContext context)
+        public override Task<MessageReply> PostProfile(ProfileDTO request, ServerCallContext context)
         {
             Console.WriteLine("Se intenta crear usuario con id {0}", request.Id);
-            var message = serverCommands.SaveNewUserProfileAsync(request.Id + "/#" + request.Description + "/#" +"" +"/#" + request.Image);
-            //var message = serverCommands.SaveNewUserProfileAsync(request.Id + "/#" + request.Description + "/#" + request.Abilities + "/#" + request.Image);
+            string[] skills = request.Abilities.Split("/");
+            var message = serverCommands.SaveNewUserProfileAsync(request.Id + "/#" + request.Description + "/#" + skills +"/#" + request.Image);
             return Task.FromResult(new MessageReply { Message = message.Result });
         }
 
         public override Task<MessageReply> PutProfile(ProfileDTO request, ServerCallContext context)
         {
             Console.WriteLine("Se intenta actualizar usuario con nombre {0}", request.Id);
-            var message = serverCommands.UploadUserProfileAsync(request.Id + "/#" + request.Description + "/#" + "" + "/#" + request.Image);
+            string[] skills = request.Abilities.Split("/");
+            var message = serverCommands.UploadUserProfileAsync(request.Id + "/#" + request.Description + "/#" + skills + "/#" + request.Image);
             return Task.FromResult(new MessageReply { Message = message.Result });
         }
 
         public override Task<MessageReply> DeleteProfile(ProfileDTO request, ServerCallContext context)
         {
             Console.WriteLine("Se intenta eliminar usuario con nombre {0}", request.Id);
-            var message = serverCommands.DeleteUserProfileAsync(request.Id + "/#" + request.Description + "/#" + "" + "/#" + request.Image);
+            string[] skills = request.Abilities.Split("/");
+            var message = serverCommands.DeleteUserProfileAsync(request.Id + "/#" + request.Description + "/#" + skills + "/#" + request.Image);
             return Task.FromResult(new MessageReply { Message = message.Result });
         }
 
         public Task<MessageReply> PutImageProfile(ProfileDTO request, ServerCallContext context)
         {
             Console.WriteLine("Se intenta actualizar usuario con nombre {0}", request.Id);
-            //var message = serverCommands.UploadUserProfileImageAsync(request.Id + "/#" + request.Description + "/#" + request.Abilities + "/#" + request.Image);
-            var message = serverCommands.UploadUserProfileImageAsync(request.Id + "/#" + request.Description + "/#" + "" + "/#" + request.Image);
+            string[] skills = request.Abilities.Split("/");
+            var message = serverCommands.UploadUserProfileImageAsync(request.Id + "/#" + request.Description + "/#" + skills + "/#" + request.Image);
             return Task.FromResult(new MessageReply { Message = message.Result });
         }
     }
